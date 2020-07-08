cf085c146f99ae303cb062a59d4e84ba0790c7d4
KaiWei Shen <keshen@ucdavis.edu>
2020-07-07 22:53:20 -0700

    finished product

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+USE burger_db; 
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Avacado Burger'); 
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,59 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newdevour = $(this).data("newdevour");
+
+        var newdevourState = {
+            devoured: newdevour
+        };
+
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newdevourState
+        }).then(
+            function () {
+                console.log("changed sleep to", newdevour);
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function (event) {
+
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#ca").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function (event) {
+        var id = $(this).data("id");
+
+        // Send the DELETE request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted burger", id);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
